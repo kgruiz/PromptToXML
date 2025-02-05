929b405da929b85ce1c204767e0ae189e0fd550c
kgruiz <kgruiz@umich.edu>
2025-02-04 19:28:17 -0500

    Fully fixed app

--- Shared (App)/Resources/Script.js
+++ Shared (App)/Resources/Script.js
@@ -2,13 +2,17 @@ function show(platform, enabled, useSettingsInsteadOfPreferences) {
     document.body.classList.add(`platform-${platform}`);
 
     if (useSettingsInsteadOfPreferences) {
-        document.getElementsByClassName('platform-mac state-on')[0].innerText = "test’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-off')[0].innerText = "test’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-unknown')[0].innerText = "You can turn on test’s extension in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac open-preferences')[0].innerText = "Quit and Open Safari Settings…";
+        document.getElementsByClassName('platform-mac state-on')[0].innerText =
+            'PromptToXML’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.';
+        document.getElementsByClassName('platform-mac state-off')[0].innerText =
+            'PromptToXML’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.';
+        document.getElementsByClassName('platform-mac state-unknown')[0].innerText =
+            'You can turn on PromptToXML’s extension in the Extensions section of Safari Settings.';
+        document.getElementsByClassName('platform-mac open-preferences')[0].innerText =
+            'Quit and Open Safari Settings…';
     }
 
-    if (typeof enabled === "boolean") {
+    if (typeof enabled === 'boolean') {
         document.body.classList.toggle(`state-on`, enabled);
         document.body.classList.toggle(`state-off`, !enabled);
     } else {
@@ -18,7 +22,8 @@ function show(platform, enabled, useSettingsInsteadOfPreferences) {
 }
 
 function openPreferences() {
-    webkit.messageHandlers.controller.postMessage("open-preferences");
+    webkit.messageHandlers.controller.postMessage('open-preferences');
 }
 
-document.querySelector("button.open-preferences").addEventListener("click", openPreferences);
+document.querySelector('button.open-preferences')
+    .addEventListener('click', openPreferences);
